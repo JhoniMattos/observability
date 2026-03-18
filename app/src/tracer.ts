9e7ac1773b738e22de4b7f9c2764bcee850c2626
jhoni <jhoni.psy@hotmail.com>
2026-03-18 13:14:21 +0000

    new: add prometheus and change the versions

--- app/src/tracer.ts
+++ app/src/tracer.ts
@@ -1,23 +1,24 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import {
-  BatchSpanProcessor,
-  ConsoleSpanExporter,
-} from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import {
-  PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
-} from '@opentelemetry/sdk-metrics';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 import { resourceFromAttributes } from '@opentelemetry/resources';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import {
   ATTR_SERVICE_NAME,
   ATTR_SERVICE_VERSION,
 } from '@opentelemetry/semantic-conventions';
-import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs';
-import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-proto';
 import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 
+const SERVICE_NAME = 'app-rocketseat';
+
+const metricsExporter = new OTLPMetricExporter({
+  url: 'http://127.0.0.1:4317',
+});
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricsExporter,
+  exportIntervalMillis: 10000,
+});
 const traceExporter = new OTLPTraceExporter();
 
 const resource = resourceFromAttributes({
@@ -29,14 +30,11 @@ const mergedResource = resource;
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
 const sdk = new NodeSDK({
-  spanProcessor: new BatchSpanProcessor(traceExporter),
-  traceExporter: new ConsoleSpanExporter(),
-  metricReader: new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  }),
-  logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogExporter()),
+  traceExporter,
+  metricReader,
   instrumentations: [getNodeAutoInstrumentations()],
   resource: mergedResource,
+  serviceName: SERVICE_NAME,
 });
 
 export default sdk;
